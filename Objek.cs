cbdaa01a6f136f8e1c35ccb617699ce892fd32f5
yonasdangapaelongan <yonasdanga@gmail.com>
2020-03-21 11:05:09 -0700

    taksi.cs

---
+++ Objek.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace taksi
+{
+    class Objek
+    {
+        string nama;
+        int nomor;
+        int duty;
+
+        public void DriverName()
+        {
+            Console.WriteLine("Driver Name : Jono");
+        }
+
+        public void OnDuty()
+        {
+            Console.WriteLine("On Duty : Yes");
+        }
+
+        public void NumberOfPpassenger()
+        {
+            Console.WriteLine("Number Of Passenger : 10");
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("Jono Sedang Menjemput Penumpang");
+        }
+
+        public void DropOffPasengger()
+        {
+            Console.WriteLine("Jono Sedang Mengantar Penumpang");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace taksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            Objek taksi = new Objek();
+
+            taksi.DriverName();
+            taksi.OnDuty();
+            taksi.NumberOfPpassenger();
+
+           
+            taksi.PickUpPassenger();
+            taksi.DropOffPasengger();
+
+            Console.ReadKey();
+            
+        }
+    }
+}
